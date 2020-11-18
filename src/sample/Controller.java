d4eb7ac25ccc44a63b78307eac954fb4f376b00c
Alexander Wiklund <alexander.wiklund1@gmail.com>
2020-11-18 14:46:17 +0100

    drawboard method

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,26 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
 
+    //Where the buttons are drawn
+    @FXML
+    GridPane board;
+
+    public void drawboard(byte length, byte height) {
+        for(byte x = 0; x < length; x++) {
+            for(byte y = 0; y < height; y++) {
+                //Creates a new button
+                Button currentButton = new Button();
+
+                //Adds button at (x,y) coordinate in the gridpane
+                board.add(currentButton, x, y);
+            }
+        }
+
+    }
 }
 
